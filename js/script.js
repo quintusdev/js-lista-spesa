887e0ff53993fe5eb63914b114bcccb3d2a077cb
quintusdev <quintusdev92@gmail.com>
2023-05-24 14:19:24 +0200

    ES. COMPLETATO

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //SCRIVERE LA LISTA DELLA SPESA CON CICLO WHILE
 
 // Creo un array con gli elementi della lista della spesa
-const listaSpesa = ['Pasta', 'Uovo', 'Guanciale', 'Pecorino', 'Sale', 'Pepe Nero', 'Acqua'];
+const listaSpesa = ['Pasta', 'Uovo', 'Guanciale', 'Pecorino', 'Sale', 'Pepe Nero', 'Acqua', 'Altro Vino'];
 
 // Ottengo il riferimento all'elemento <ul> della lista della spesa nel documento HTML
 const lista = document.getElementById('lista_spesa');
@@ -11,7 +11,7 @@ let counter = 0;
 
 // Utilizzo un ciclo while per generare gli elementi della lista della spesa nell'HTML
 while (counter < listaSpesa.length) {
-    
+
     // Costruisco il markup HTML per un singolo elemento della lista
     let item = `<li>${listaSpesa[counter]}</li>`;
     
