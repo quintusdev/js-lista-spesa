e6ebc3863f9c0909305359a78284a859cb9acd5a
quintusdev <quintusdev92@gmail.com>
2023-05-24 14:05:30 +0200

    [STYLE]Stilizzazione pagina

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 //SCRIVERE LA LISTA DELLA SPESA CON CICLO WHILE
 
-//CREO ARRAY CON LA LISTA DELLA SPESA
+// Creo un array con gli elementi della lista della spesa
 const listaSpesa = ['Pasta', 'Uovo', 'Guanciale', 'Pecorino', 'Sale', 'Pepe Nero', 'Acqua'];
 
-//RICHIAMO L'ELEMENTO UL 
+// Ottengo il riferimento all'elemento <ul> della lista della spesa nel documento HTML
 const lista = document.getElementById('lista_spesa');
 
+// Inizializzo un contatore per tenere traccia dell'indice corrente nell'array
 let counter = 0;
 
+// Utilizzo un ciclo while per generare gli elementi della lista della spesa nell'HTML
 while (counter < listaSpesa.length) {
-      
+    
+    // Costruisco il markup HTML per un singolo elemento della lista
     let item = `<li>${listaSpesa[counter]}</li>`;
+    
+    // Aggiungo l'elemento alla lista della spesa nell'HTML
     lista.innerHTML += item;
+    
+    // Incremento il contatore per passare all'elemento successivo nell'array
     counter++;
 }
