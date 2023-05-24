a9d679d419d9c3b03177af60d7e48797b4f26ad2
quintusdev <quintusdev92@gmail.com>
2023-05-24 13:38:00 +0200

    [FEATURES] ES. COMPLETATO

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 //SCRIVERE LA LISTA DELLA SPESA CON CICLO WHILE
 
+//CREO ARRAY CON LA LISTA DELLA SPESA
+const listaSpesa = ['Pasta', 'Uovo', 'Guanciale', 'Pecorino', 'Sale', 'Pepe Nero', 'Acqua'];
+
+//RICHIAMO L'ELEMENTO UL 
+const lista = document.getElementById('lista_spesa');
+
+let counter = 0;
+
+while (counter < listaSpesa.length) {
+      
+    let item = `<li>${listaSpesa[counter]}</li>`;
+    lista.innerHTML += item;
+    counter++;
+}
